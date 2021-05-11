026c82970391e0d5428ddba7c94b98104f1e5326
eniompw <71254710+eniompw@users.noreply.github.com>
2021-05-11 09:28:31 +0100

    Update flask_app.py

--- flask_app.py
+++ flask_app.py
@@ -1,6 +1,22 @@
 from flask import Flask
 app = Flask(__name__)
 
+import os
+import psycopg2
+DATABASE_URL = os.environ['DATABASE_URL']
+conn = psycopg2.connect(DATABASE_URL, sslmode='require')
+
 @app.route('/')
 def hello_world():
     return 'Postgres'
+
+@app.route('/create')
+def create():
+	cur = conn.cursor()
+	cur.execute(	"""	CREATE TABLE Users(
+					Username VARCHAR(20) NOT NULL PRIMARY KEY,
+					Password VARCHAR(20) NOT NULL
+						                  )
+			""")
+	con.commit()
+	return 'CREATE'
